f331afe128a19451dbdad1171a5d78ab257b6257
j_r_style@hotmail.com <>
2018-07-21 10:30:33 -0300

    COMITA PFVR, nunca te pedi nads!!!

---
+++ src/xPertCore/XPert.java
@@ -0,0 +1,57 @@
+package xPertCore;
+
+import java.util.ArrayList;
+import java.lang.String;
+
+
+public class XPert {
+	private String Diretorio;
+	private ArrayList<Projeto> projetosAbertos = new ArrayList<Projeto>(0);
+	
+	public void abreProjeto(String nome)
+	{
+		//Pega um projeto presente no Diret�rio e o coloca na mem�ria
+	}
+	public void fechaProjeto(String nome)
+	{
+		int n = projetosAbertos.size();
+		
+		for(int c=0;c<n;c++)
+		{
+			if((projetosAbertos.get(c)).getNome() == nome)
+			{
+				//Atualizar o banco de dados...
+				projetosAbertos.remove(c);
+				return;
+			}
+		}
+		
+		System.out.println("Projeto "+nome+" nao esta aberto" );
+		return;
+	}
+	public Projeto getProjeto(String nome)
+	{
+		int n = projetosAbertos.size();
+		
+		for(int c=0;c<n;c++)
+		{
+			if((projetosAbertos.get(c)).getNome() == nome)
+				return projetosAbertos.get(c);
+		}
+		
+		System.out.println("Projeto "+nome+" nao esta aberto" );
+		return null;
+	}
+	public String getDiretorio()
+	{
+		return Diretorio;
+	}
+	public void setDiretorio(String dir)
+	{
+		if(dir != null)
+			Diretorio = dir;
+		else
+			System.out.println("Diretorio Vazio");
+	}
+	
+}
